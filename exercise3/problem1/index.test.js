374e47c77bc641e08e11c4e54ec1740a11d99414
Talgat <talgat.s@protonmail.com>
2023-06-27 15:20:34 +0600

    add exercise3

---
+++ exercise3/problem1/index.test.js
@@ -0,0 +1,30 @@
+const { describe, expect, test } = require("@jest/globals");
+const concat = require("./index");
+
+describe("exercise2 - problem1", () => {
+  test.each([
+    {
+      args: [
+        [1, 2, 3],
+        [4, 5],
+        [6, 7],
+      ],
+      expected: [1, 2, 3, 4, 5, 6, 7],
+    },
+    {
+      args: [[1], [2], [3], [4], [5], [6], [7]],
+      expected: [1, 2, 3, 4, 5, 6, 7],
+    },
+    {
+      args: [
+        [1, 2],
+        [3, 4],
+      ],
+      expected: [1, 2, 3, 4],
+    },
+    { args: [[4, 4, 4, 4, 4]], expected: [4, 4, 4, 4, 4] },
+    { args: [["a"], ["b", "c"]], expected: ["a", "b", "c"] },
+  ])(".concat(...$args)", ({ args, expected }) => {
+    expect(concat(...args)).toEqual(expected);
+  });
+});
