407832ebb7c80a4400608555fe7a5b0d823845e2
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 11:48:24 +0500

    Update index.js

--- exercise1/problem9/index.js
+++ exercise1/problem9/index.js
@@ -1,5 +1,15 @@
-function isDisarium() {
-  // Your code
+function isDisarium(number) {
+      const numStr = number.toString();
+    
+    let sum = 0;
+    
+    for (let i = 0; i < numStr.length; i++) {
+        const digit = parseInt(numStr[i]);
+        const position = i + 1;
+        sum += Math.pow(digit, position);
+    }
+    
+    return sum === number;
 }
 
 module.exports = isDisarium;
