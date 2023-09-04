62add598aae828565f083aa535494a9f1924814c
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 17:48:45 +0500

    Update index.js

--- exercise1/problem5/index.js
+++ exercise1/problem5/index.js
@@ -6,4 +6,11 @@ function removeABC(str) {
 
     return modifiedStr;
 }
+
+console.log(removeABC("This might be a bit hard")) // "This might e  it hrd"
+
+console.log(removeABC("hello world!")) // null
+
+console.log(removeABC("")) // null
+
 module.exports = removeABC;
