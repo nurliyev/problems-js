d63eb8f57f058126d4c92da166f1408d8837fc56
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 10:57:36 +0500

    Update index.js

--- exercise1/problem8/index.js
+++ exercise1/problem8/index.js
@@ -1,5 +1,18 @@
-function longestTime() {
-  // Your code
+function longestTime(h, m, s) {
+
+    const hours = h * 3600;
+    const minutes = m * 60;
+    const seconds = s;
+
+    const maxDuration = Math.max(hours, minutes, seconds);
+
+    if (maxDuration === hours) {
+        return h;
+    } else if (maxDuration === minutes) {
+        return m;
+    } else {
+        return s;
+    }
 }
 
 module.exports = longestTime;
