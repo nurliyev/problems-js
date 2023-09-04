748f14248f7c5d62bd90fba8c4f2cc7d2f121699
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 17:50:16 +0500

    Update index.js

--- exercise1/problem9/index.js
+++ exercise1/problem9/index.js
@@ -12,4 +12,18 @@ function isDisarium(number) {
     return sum === number;
 }
 
+console.log(isDisarium(75)) // false
+// 7^1 + 5^2 = 7 + 25 = 32
+
+console.log(isDisarium(135)) // true
+// 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+
+console.log(isDisarium(544)) // false
+
+console.log(isDisarium(518)) // true
+
+console.log(isDisarium(8)) // true
+
+console.log(isDisarium(466)) // false
+
 module.exports = isDisarium;
