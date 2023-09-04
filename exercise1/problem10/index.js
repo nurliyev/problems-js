2e51e9bba526c7679aff2b2c26f51c8d38ef29ce
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 12:18:09 +0500

    Update index.js

--- exercise1/problem10/index.js
+++ exercise1/problem10/index.js
@@ -1,5 +1,33 @@
-function move() {
-  // Your code
-}
+function move(str) {
+    let result = '';
 
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+
+        // Check if the character is a lowercase letter
+        if (char >= 'a' && char <= 'z') {
+            // Shift the letter to the next one
+            char = String.fromCharCode(char.charCodeAt(0) + 1);
+
+            // Handle 'z' by wrapping around to 'a'
+            if (char > 'z') {
+                char = 'a';
+            }
+        } else if (char >= 'A' && char <= 'Z') {
+            // Check if the character is an uppercase letter
+            // Shift the letter to the next one
+            char = String.fromCharCode(char.charCodeAt(0) + 1);
+
+            // Handle 'Z' by wrapping around to 'A'
+            if (char > 'Z') {
+                char = 'A';
+            }
+        }
+
+        // Append the modified character to the result
+        result += char;
+    }
+
+    return result;
+}
 module.exports = move;
