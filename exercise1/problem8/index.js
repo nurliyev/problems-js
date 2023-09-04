4ef358745689bcd7070d26f2ddc262e06cb165f7
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 17:49:41 +0500

    Update index.js

--- exercise1/problem8/index.js
+++ exercise1/problem8/index.js
@@ -15,4 +15,10 @@ function longestTime(h, m, s) {
     }
 }
 
+console.log(longestTime(1, 59, 3598)) // 1
+
+console.log(longestTime(2, 300, 15000)) // 300
+
+console.log(longestTime(15, 955, 59400)) // 59400
+
 module.exports = longestTime;
