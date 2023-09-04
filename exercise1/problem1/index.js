96df2c78c8a19262611f286631d61fb997e39bb6
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 17:45:47 +0500

    Update index.js

--- exercise1/problem1/index.js
+++ exercise1/problem1/index.js
@@ -3,4 +3,12 @@ function numOfDigits(int) {
     return numStr.length;
 }
 
+console.log(numOfDigits(1000)) // 4
+
+console.log(numOfDigits(12)) // 2
+
+console.log(numOfDigits(1305981031)) // 10
+
+console.log(numOfDigits(0)) // 1
+
 module.exports = numOfDigits;
