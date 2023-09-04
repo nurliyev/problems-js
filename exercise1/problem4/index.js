8ffe6b9c37406cf3580671efece10f19538fad33
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 17:48:13 +0500

    Update index.js

--- exercise1/problem4/index.js
+++ exercise1/problem4/index.js
@@ -9,4 +9,12 @@ function makeTitleCase(str)
   return words.join(' ');
 }
 
+console.log(makeTitleCase("This is a title")) // "This Is A Title"
+
+console.log(makeTitleCase("capitalize every word")) // "Capitalize Every Word"
+
+console.log(makeTitleCase("I Like Pizza")) // "I Like Pizza"
+
+console.log(makeTitleCase("PIZZA PIZZA PIZZA")) // "PIZZA PIZZA PIZZA"
+
 module.exports = makeTitleCase;
