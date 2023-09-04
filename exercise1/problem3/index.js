903246af0f58002da027a8cf47e3470aa6eabdbe
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 17:47:40 +0500

    Update index.js

--- exercise1/problem3/index.js
+++ exercise1/problem3/index.js
@@ -7,4 +7,12 @@ function pentagonal(num) {
 	return sum;
 }
 
+console.log(pentagonal(1)) // 1
+
+console.log(pentagonal(2)) // 6
+
+console.log(pentagonal(3)) // 16
+
+console.log(pentagonal(8)) // 141
+
 module.exports = pentagonal;
