22179a4fa504c37dfd05f3836263c01601305a31
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 09:17:11 +0500

    Update index.js

--- exercise1/problem4/index.js
+++ exercise1/problem4/index.js
@@ -1,5 +1,12 @@
-function makeTitleCase() {
-  // Your code
+function makeTitleCase(str)
+{
+  let array1 = str.split(' ');
+  let words = [];
+    
+  for(let x = 0; x < array1.length; x++){
+      words.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+  return words.join(' ');
 }
 
 module.exports = makeTitleCase;
