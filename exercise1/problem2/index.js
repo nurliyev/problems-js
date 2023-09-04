807d4d910a9e07102279d53c1c8e49eb7c8d4b08
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 17:46:31 +0500

    Update index.js

--- exercise1/problem2/index.js
+++ exercise1/problem2/index.js
@@ -2,4 +2,10 @@ function intWithinBounds(n, lower, upper) {
   return Number.isInteger(n) && n >= lower && n < upper;
 }
 
+console.log(intWithinBounds(3, 1, 9)) // true
+
+console.log(intWithinBounds(6, 1, 6)) // false
+
+console.log(intWithinBounds(4.5, 3, 8)) // false
+
 module.exports = intWithinBounds;
