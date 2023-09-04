6eeeab5470bafce1aaf118af70d23372af3af632
nurliyev <104318761+nurliyev@users.noreply.github.com>
2023-09-04 09:00:58 +0500

    Update index.js

--- exercise1/problem3/index.js
+++ exercise1/problem3/index.js
@@ -1,5 +1,10 @@
-function pentagonal() {
-  // Your code
+function pentagonal(num) {
+  	let sum = 1;
+	sum += (5*(num-1));
+	for(let i = 3;i<=num;i++){
+		sum +=(5*(i-2));
+	}
+	return sum;
 }
 
 module.exports = pentagonal;
