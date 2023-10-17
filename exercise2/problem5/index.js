c0629ebddfacb0a1af6c7c808e304175d41b51c8
nuliyev <jokerprodigy@gmail.com>
2023-10-17 11:03:52 +0500

    Commit 17-10-23

--- exercise2/problem5/index.js
+++ exercise2/problem5/index.js
@@ -1,5 +1,43 @@
-function getBudgets() {
-  // Your code
-}
+function getBudgets(arr) {
+  let totalBudget = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    totalBudget += arr[i].budget;
+  }
+
+  return totalBudget;
+}
+console.log(
+  getBudgets([
+    { name: "John", age: 21, budget: 23000 },
+    { name: "Steve", age: 32, budget: 40000 },
+    { name: "Martin", age: 16, budget: 2700 },
+  ])
+); // 65700
+
+console.log(
+  getBudgets([
+    { name: "John", age: 21, budget: 29000 },
+    { name: "Steve", age: 32, budget: 32000 },
+    { name: "Martin", age: 16, budget: 1600 },
+  ])
+); // 62600
 module.exports = getBudgets;
+
+// Create the function that takes an array with objects and returns the sum of people's budgets.
+
+// console.log(
+// 	getBudgets([
+// 		{name: "John", age: 21, budget: 23000},
+// 		{name: "Steve", age: 32, budget: 40000},
+// 		{name: "Martin", age: 16, budget: 2700},
+// 	])
+// ); // 65700
+
+// console.log(
+// 	getBudgets([
+// 		{name: "John", age: 21, budget: 29000},
+// 		{name: "Steve", age: 32, budget: 32000},
+// 		{name: "Martin", age: 16, budget: 1600},
+// 	])
+// ); // 62600
--- exercise2/problem6/index.js
+++ exercise2/problem6/index.js
@@ -1,5 +1,23 @@
-function mapping() {
-  // Your code
+function mapping(letters) {
+  {
+    const result = {};
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      result[letter] = letter.toUpperCase();
+    }
+
+    return result;
+  }
 }
 
 module.exports = mapping;
+
+console.log(mapping(["p", "s"])); // { "p": "P", "s": "S" }
+
+console.log(mapping(["a", "b", "c"])); // { "a": "A", "b": "B", "c": "C" }
+
+console.log(mapping(["a", "v", "y", "z"])); // { "a": "A", "v": "V", "y": "Y", "z": "Z" }
+
+//Write a function that creates an object with each (key, value)
+//pair being the (lower case, upper case) versions of a letter, respectively.
