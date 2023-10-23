ad98da770e9c3a0f86a9fb7f53db64cf4857e26e
nuliyev <jokerprodigy@gmail.com>
2023-10-23 20:04:12 +0500

    1

--- exercise4/problem2/index.ts
+++ exercise4/problem2/index.ts
@@ -1,8 +1,43 @@
 class Name {
-  name: string;
-  constructor() {
-    this.name = "Yedil";
+  #firstName: string;
+  #lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName =
+      firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+    this.lastName =
+      lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+  }
+
+  get firstName(): string {
+    return this.firstName;
+  }
+
+  get lastName(): string {
+    return this.lastName;
+  }
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  get initials(): string {
+    return `${this.firstName.charAt(0)}.${this.lastName.charAt(0)}`;
   }
 }
 
+const p1 = new Name("john", "SMITH");
+
+console.log(p1.firstName); // "John"
+console.log(p1.lastName); // "Smith"
+console.log(p1.fullName); // "John Smith"
+console.log(p1.initials); // "J.S"
+
 export default Name;
+
+// class Name {
+//   name: string;
+//   constructor() {
+//     this.name = "Yedil";
+//   }
+// }
