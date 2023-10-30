17e525430bb58ecca8a02547848e236f365c85b0
nuliyev <jokerprodigy@gmail.com>
2023-10-30 20:41:58 +0500

    exercise4 problem7

--- exercise4/problem6/index.ts
+++ exercise4/problem6/index.ts
@@ -1,18 +1,64 @@
 // Update it as much as you want, just don't change the names
 
-export class BankAccount {}
+export class BankAccount {
+  private balance: number;
 
-export class FedexAccount {}
+  constructor() {
+    this.balance = 0;
+  }
 
-export class KazPostAccount {}
+  withdraw(amount: number): number {
+    if (amount > this.balance) {
+      return -1;
+    }
 
-export function withdrawMoney(accounts, amount) {
+    this.balance -= amount;
+    return this.balance;
+  }
+
+  deposit(amount: number): number {
+    if (amount > 1_000_000) {
+      return -1;
+    }
+
+    this.balance += amount;
+    return this.balance;
+  }
+}
+
+export class FedexAccount {
+  sendMail(recipient: string): void {
+    console.log(`Mail sent to ${recipient} via FedEx.`);
+  }
+
+  recieveMail(sender: string): void {
+    console.log(`Mail received from ${sender} via FedEx.`);
+  }
+}
+
+export class KazPostAccount extends BankAccount {
+  sendMail(recipient: string): void {
+    console.log(`Mail sent to ${recipient} via KazPost.`);
+  }
+
+  recieveMail(sender: string): void {
+    console.log(`Mail received from ${sender} via KazPost.`);
+  }
+}
+
+export function withdrawMoney(
+  accounts: BankAccount | KazPostAccount,
+  amount: number
+) {
   for (const account of accounts) {
     account.withdraw(amount);
   }
 }
 
-export function sendLetterTo(accounts, recipient) {
+export function sendLetterTo(
+  accounts: FedexAccount | KazPostAccount,
+  recipient: string
+) {
   for (const account of accounts) {
     account.sendMail(recipient);
   }
--- exercise4/problem7/index.ts
+++ exercise4/problem7/index.ts
@@ -1,3 +1,8 @@
-const is;
+const is = {
+  bool: (value: any): boolean => typeof value === "boolean",
+  num: (value: any): boolean => typeof value === "number",
+  str: (value: any): boolean => typeof value === "string",
+  fun: (value: any): boolean => typeof value === "function",
+};
 
 export default is;
