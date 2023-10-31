4240f2ee829463a7abd9ac919082ef11278b51d6
nuliyev <jokerprodigy@gmail.com>
2023-10-31 23:32:38 +0500

    problem4

--- exercise4/problem4/index.ts
+++ exercise4/problem4/index.ts
@@ -1,8 +1,34 @@
+interface Shape {
+  getArea: () => number;
+}
 // Update it as much as you want, just don't change the name
-export class Circle {}
+export class Circle {
+  radius: number;
+  constructor(radius: number) {
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+export class Rectangle {
+  width: number;
+  height: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+}
 
 // Update it as much as you want, just don't change the name
-export class Rectangle {}
+export function sumOfAllAreas(...shapes: Shape[]): number {
+  return shapes.reduce((totalArea, shape) => totalArea + shape.getArea(), 0);
+}
 
-// Update it as much as you want, just don't change the name
-export function sumOfAllAreas() {}
+const circle1 = createCircle(2);
+const circle2 = createCircle(3);
+const rect1 = createRectangle(2, 4);
+const rect2 = createRectangle(3, 2);
