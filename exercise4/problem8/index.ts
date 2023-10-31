1f354f2547707e38043dd0ab5622964cff787cb1
nuliyev <jokerprodigy@gmail.com>
2023-10-31 15:02:15 +0500

    problem8

--- exercise4/problem8/index.ts
+++ exercise4/problem8/index.ts
@@ -1,3 +1,34 @@
-class Calculator {}
+class Calculator {
+  result: number;
+
+  constructor(initialValue: number) {
+    this.result = initialValue;
+  }
+
+  add(num: number) {
+    this.result += num;
+    return this;
+  }
+
+  subtract(num: number) {
+    this.result -= num;
+    return this;
+  }
+
+  divide(num: number) {
+    this.result /= num;
+    return this;
+  }
+
+  multiply(num: number) {
+    this.result *= num;
+    return this;
+  }
+
+  logResult(num: number) {
+    console.log(this.result);
+    return this;
+  }
+}
 
 export default Calculator;
