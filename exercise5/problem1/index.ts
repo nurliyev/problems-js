5609575aabd859cfcf8ad08567fa12f8ef9e105c
nuliyev <jokerprodigy@gmail.com>
2023-11-06 23:43:55 +0500

    exercise5

--- exercise5/problem1/index.ts
+++ exercise5/problem1/index.ts
@@ -1,3 +1,11 @@
-function callbackExec() {}
+function callbackExec(callback: () => void): void {
+  setTimeout(callback, 2000); // 2000 milliseconds (2 seconds)
+}
+
+function displayHello() {
+  console.log("Hello");
+}
+
+callbackExec(displayHello); // Calls `displayHello` after 2 seconds
 
 export default callbackExec;
--- exercise5/problem2/index.ts
+++ exercise5/problem2/index.ts
@@ -1,3 +1,10 @@
-function printNumberInInterval() {}
-
+function printNumberInInterval(num: number) {
+  const counter = setInterval(() => {
+    if (num === 0) {
+      clearInterval(counter);
+    }
+    console.log(num--);
+  }, 1000);
+}
+printNumberInInterval(10);
 export default printNumberInInterval;
