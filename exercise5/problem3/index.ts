d5078426ec357fc83b55eba239c594826167f5b6
nuliyev <jokerprodigy@gmail.com>
2023-11-07 22:29:10 +0500

    exercise3

--- exercise5/problem3/index.ts
+++ exercise5/problem3/index.ts
@@ -1,3 +1,12 @@
-function printAsyncNumbers() {}
+function printAsyncNumbers(n: number) {
+  let second = 1;
+
+  for (let i = 1; i <= n; i++) {
+    setTimeout(() => {
+      console.log(`after ${i} sec - ${i}`);
+    }, second * 1000);
+    second++;
+  }
+}
 
 export default printAsyncNumbers;
