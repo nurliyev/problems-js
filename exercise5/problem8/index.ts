819c622da15420cca5377b4ca1dc565a121f1040
nuliyev <jokerprodigy@gmail.com>
2023-11-17 21:09:08 +0500

    exercise8

--- exercise5/problem8/index.ts
+++ exercise5/problem8/index.ts
@@ -1,5 +1,28 @@
-async function fetchCharacterWithMovies() {
-  // Your code
+type nameofActor = {
+  name: number;
+  films: {
+    string;
+  };
+};
+
+async function fetchCharacterWithMovies(name: string) {
+  try {
+    const response = await fetch(`https://swapi.dev/api/${name}`);
+
+    if (!response.ok) {
+      //console.error(`Error: HTTP status ${response.status}`);
+      await Promise.reject(new Error("some error"));
+    }
+
+    const json: nameofActor = await response.json();
+
+    return {
+      name: json.name,
+      films: json.films,
+    };
+  } catch (error) {
+    return Promise.reject(new Error("smth went wrong"));
+  }
 }
 
 export default fetchCharacterWithMovies;
