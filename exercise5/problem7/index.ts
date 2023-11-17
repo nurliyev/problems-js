7308ff6fc474af2f93c009791cae74b0c39b6ac0
nuliyev <jokerprodigy@gmail.com>
2023-11-17 18:51:43 +0500

    exercise7

--- exercise5/problem7/index.ts
+++ exercise5/problem7/index.ts
@@ -10,42 +10,43 @@ type Pokemon = {
 
 async function fetchPokemons(pokemons: string[]) {
   try {
-    const result = [];
+    const resultPromise = [];
+
     for (const pokemon: string of pokemons) {
-      const pokemonObject = await fetchPokemon(pokemon);
-      result.push(pokemonObject);
+      const promise = fetchPokemon(pokemon);
+      resultPromise.push(promise);
     }
 
+    const results = [];
+
+    for (const promise: string of resultPromise) {
+      const result = await promise;
+      results.push(result);
     }
 
-    return result;
-
+    return results;
   } catch (error) {
     return Promise.reject(new Error("smth went wrong"));
   }
 }
 
 async function fetchPokemon(name: string) {
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/berry/${name}`);
+  const response = await fetch(`https://pokeapi.co/api/v2/berry/${name}`);
 
-    if (!response.ok) {
-      //console.error(`Error: HTTP status ${response.status}`);
-      await Promise.reject(new Error("some error"));
-    }
-
-    const json: Pokemon = await response.json();
-
-    return {
-      id: json.id,
-      name: json.name,
-      height: json.height,
-      weight: json.weight,
-      image: json.sprites.front_default,
-    };
-  } catch (error) {
-    return Promise.reject(new Error("smth went wrong"));
+  if (!response.ok) {
+    //console.error(`Error: HTTP status ${response.status}`);
+    await Promise.reject(new Error("some error"));
   }
+
+  const json: Pokemon = await response.json();
+
+  return {
+    id: json.id,
+    name: json.name,
+    height: json.height,
+    weight: json.weight,
+    image: json.sprites.front_default,
+  };
 }
 
 export default fetchPokemons;
