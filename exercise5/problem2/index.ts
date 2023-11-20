04ee3e2ba33ffa271513c873c93a415f2cbad53d
nuliyev <jokerprodigy@gmail.com>
2023-11-20 10:09:46 +0500

    exercise5

--- exercise5/problem2/index.ts
+++ exercise5/problem2/index.ts
@@ -1,10 +1,19 @@
 function printNumberInInterval(num: number) {
-  const counter = setInterval(() => {
-    if (num === 0) {
-      clearInterval(counter);
-    }
-    console.log(num--);
-  }, 1000);
+  let i = 1;
+  const intervalId = setInterval(callback: () => {
+    if (i >= num; ) {
+      clearInterval(intervalId);
+      return;
+    } 
+    console.log(i);
+    i++;
+  }, 1000 )
+  // const counter = setInterval(() => {
+  //   if (num === 0) {
+  //     clearInterval(counter);
+  //   }
+  //   console.log(num--);
+  // }, 1000);
 }
 printNumberInInterval(10);
 export default printNumberInInterval;
