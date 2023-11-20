746ba698a5ba00b667d67d1a2103c5854f7e2dfc
nuliyev <jokerprodigy@gmail.com>
2023-11-20 21:52:22 +0500

    exercise5

--- exercise5/problem3/index.ts
+++ exercise5/problem3/index.ts
@@ -1,12 +1,22 @@
 function printAsyncNumbers(n: number) {
-  let second = 1;
+  printNumbersRec(n, 1);
+  // let second = 1;
+  // for (let i = 1; i <= n; i++) {
+  //   setTimeout(() => {
+  //     console.log(`after ${i} sec - ${i}`);
+  //   }, second * 1000);
+  //   second++;
+  // }
+}
 
-  for (let i = 1; i <= n; i++) {
-    setTimeout(() => {
-      console.log(`after ${i} sec - ${i}`);
-    }, second * 1000);
-    second++;
-  }
+function printNumbersRec(n: number, i: number) {
+  setTimeout(() => {
+    if (i > n) {
+      return;
+    }
+    console.log(i);
+    printNumbersRec(n, i + 1);
+  }, i * 1000);
 }
 
 export default printAsyncNumbers;
--- exercise5/problem4/index.ts
+++ exercise5/problem4/index.ts
@@ -1,3 +1,31 @@
-class Timer {}
+class Timer {
+  first: Date = new Data();
+
+  start() {
+    this.first = new Date();
+  }
+
+  pause();
+  reset();
+  log() {
+    const now = new Date();
+    const diff = this.first?.getTime() - now.getTime();
+    const formated = formatTime(diff);
+    console.log(formated);
+  }
+}
+
+function formatTime(totalMs: number) {
+  const h = Math.floor(totalMs / (60 * 60 * 1000));
+  const remainingM = totalMs % (60 * 60 * 1000);
+  const m = Math.floor(remainingM / (60 * 1000));
+  const remainingS = remainingM % (60 * 1000);
+  const s = Math.floor(remainingM / 1000);
+  const ms = remainingS % 1000;
+
+  return { h, m, s, ms };
+}
+
+console.log(formatTime(3 * 60 * 60 * 1000 + 20 * 60 * 1000 + 13 * 1000 + 150));
 
 export default Timer;
